fc17ed06c796a08c424d50b38653eba9dc097402
Allvin <allvin97@gmail.com>
2025-11-12 20:04:35 +0530

    array practice

---
+++ src/main/java/com/example/springLearn/Practice/ArraysPractice.java
@@ -0,0 +1,100 @@
+package com.example.springLearn.Practice;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+
+@Slf4j
+public class ArraysPractice {
+
+    public static void main(String[] args) {
+
+        /*
+            1. Find the largest and smallest element in an array
+            2. Find the second-largest element
+            3. Find the sum and average of all elements
+            4. Reverse an array
+            5. Check if the array is sorted
+            6. Count even and odd numbers in an array
+            7. Copy one array to another
+            8. Find the maximum and minimum difference between elements
+         */
+
+        int [] arr = new int[5];
+        int arrLength = arr.length;
+        for(int i=0; i< arrLength; i++){
+            arr[i] = i+1;
+        }
+
+        int largest = arr[0];
+        int smallest = arr[0];
+        int secondLargest = 0;
+        int sum = 0;
+        int countEven = 0;
+        int countOdd = 0;
+
+        for (int j : arr) {
+            if (largest < j) {
+                secondLargest = largest;
+                largest = j;
+            }
+            if (smallest > j) {
+                smallest = j;
+            }
+            sum += j;
+            if(j%2 == 0)countEven ++;
+            else countOdd++;
+        }
+        log.info("largest : {} - smallest : {}", largest, smallest);
+        log.info("second largest : {}", secondLargest);
+        log.info("sum : {} - average : {}", sum, sum/arrLength);
+        log.info("even count : {} - odd count : {}", countEven, countOdd);
+
+        int [] arrReverse = new int[5];
+        for(int i=arrLength-1 ; i>=0 ; i--){
+            arrReverse[arrLength-1-i] = arr[i];
+        }
+        log.info("reverse array {}", IntStream.of(arrReverse)
+                .boxed()
+                .map(String::valueOf)
+                .collect(Collectors.joining(",")));
+
+        log.info("is array sorted : {}", isSorted(arr));
+        int [] newArr = {2,6,9,1,6};
+        log.info("is newArr sorted : {}", isSorted(newArr));
+
+        var copyArr1 = Arrays.copyOf(arr,arrLength);
+        var copyArr2 = Arrays.stream(arr).toArray();
+
+        log.info("copy arr 1 array {}", IntStream.of(copyArr1).boxed().map(String::valueOf).collect(Collectors.joining(",")));
+        log.info("copy arr 2 array {}", IntStream.of(copyArr2).boxed().map(String::valueOf).collect(Collectors.joining(",")));
+
+        int[] part = IntStream.range(0, 3).map(i -> arr[i]).toArray();
+        log.info("part array {}", IntStream.of(part).boxed().map(String::valueOf).collect(Collectors.joining(",")));
+
+        Arrays.sort(arr);
+        var maxDiff = arr[arrLength-1]- arr[0];
+        var minDiff = largest;
+        for(int i=0; i< arrLength-1; i++ ){
+            int diff = arr[i+1] - arr[i];
+            if(diff < minDiff){
+                minDiff = diff;
+            }
+        }
+        log.info("minimum difference : {}", minDiff);
+        log.info("maximum difference : {}", maxDiff);
+
+    }
+
+    public static boolean isSorted(int[] arr){
+        for(int i=0; i < arr.length-1; i++){
+            if(arr[i]>arr[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
